4604d1e4182e947eb27f4de2b304cc7e0e0339b6
jpneira2000 <jpneira2000@gmail.com>
2023-12-10 17:33:55 -0600

    Hotfix del post character strengths endpoint

--- services/character_strength.py
+++ services/character_strength.py
@@ -1,12 +1,12 @@
 from models.character_strength import CharacterStrengthModel
-from services.character_strength import CharacterStrengthService
+from schemas.character_strength import CharacterStrengthSchema
 
 class CharacterStrengthService():
 
     def __init__(self, db) -> None:
         self.db = db
 
-    def create_character_strength(self, character_strength: CharacterStrengthService):
+    def create_character_strength(self, character_strength: CharacterStrengthSchema):
         new_character_strength = CharacterStrengthModel(**character_strength.model_dump())
         self.db.add(new_character_strength)
         self.db.commit()
